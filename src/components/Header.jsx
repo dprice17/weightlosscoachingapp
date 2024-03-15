f7d8a0b6f1cab2e20f062769ab7404cda79ceefb
dprice17 <109916870+dprice17@users.noreply.github.com>
2024-03-15 10:53:58 -0400

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-//import logoImage from "/src/components/Weighing-Machine.svg"
+import logoImage from "/src/components/Weighing-Machine.svg"
 
 export default function Header(){
     return (
         <header>
             <h1>MyWeight
-                    <img className="header-h1-img"/>
+                    <img className="header-h1-img" src={logoImage}/>
                 LossCoach
             </h1>
             <p className="header-subtext">Weight loss advice at your fingertips</p>
